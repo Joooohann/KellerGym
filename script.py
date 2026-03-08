42015bccbac90828ceea8d694105748fe823092b
Johann <86495224+Joooohann@users.noreply.github.com>
2026-03-08 22:45:45 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,16 @@
+from html.parser import HTMLParser
+class MyParser(HTMLParser):
+  def __init__(self):
+    super().__init__()
+    self.stack = []
+    self.void = {'area','base','br','col','embed','hr','img','input','link','meta','param','source','track','wbr'}
+  def handle_starttag(self, tag, attrs):
+    if tag not in self.void: self.stack.append((tag, self.getpos()[0]))
+  def handle_endtag(self, tag):
+    if tag in self.void: return
+    if self.stack and self.stack[-1][0] == tag:
+      self.stack.pop()
+    else:
+      print(f'Line {self.getpos()[0]}: Found </{tag}> but expected {self.stack[-1]}')
+parser=MyParser()
+parser.feed(open('nutrition.html', encoding='utf-8').read())
