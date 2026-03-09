270b580be4c9ef0b61c07429e5d6cd7ea16c9dde
Johann <86495224+Joooohann@users.noreply.github.com>
2026-03-09 17:04:48 +0100

    Add files via upload

---
+++ update_dashboard.js
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('dashboard.html', 'utf8');
+content = content.replace('<div style="width:40px; height:40px; border-radius:50%; overflow: hidden; border: 1px solid #333; position: relative;">', '<a href="profile.html" style="width:40px; height:40px; border-radius:50%; overflow: hidden; border: 1px solid #333; position: relative; display: block;">');
+content = content.replace('src="img/profile.jpg"', 'src="img/icon.png"');
+content = content.replace(/<\/div>\s*<\/div>\s*<\/div>\s*<!-- Training Card/s, '</a>\n                </div>\n            </div>\n\n            <!-- Training Card');
+fs.writeFileSync('dashboard.html', content, 'utf8');
+console.log('Done!');
